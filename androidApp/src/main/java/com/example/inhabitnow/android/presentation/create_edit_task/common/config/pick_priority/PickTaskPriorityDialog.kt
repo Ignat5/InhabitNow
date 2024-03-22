60e4f2c7ee04285162c222efc278c3415ce5c006
ignatleg <ignat-2000@mail.ru>
2024-03-22 10:11:02 +0400

    feature_create_task  - pick task priority: sh + ui

---
+++ androidApp/src/main/java/com/example/inhabitnow/android/presentation/create_edit_task/common/config/pick_priority/PickTaskPriorityDialog.kt
@@ -0,0 +1,65 @@
+package com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import com.example.inhabitnow.android.presentation.base.ext.BaseScreen
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenEvent
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenResult
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenState
+import com.example.inhabitnow.android.ui.base.BaseDialogBuilder
+import com.example.inhabitnow.android.ui.base.BaseTextFiledBuilder
+
+@Composable
+fun PickTaskPriorityDialog(
+    stateHolder: PickTaskPriorityStateHolder,
+    onResult: (PickTaskPriorityScreenResult) -> Unit
+) {
+    BaseScreen(stateHolder = stateHolder, onResult = onResult) { state, onEvent ->
+        PickTaskPriorityDialogStateless(state, onEvent)
+    }
+}
+
+@Composable
+private fun PickTaskPriorityDialogStateless(
+    state: PickTaskPriorityScreenState,
+    onEvent: (PickTaskPriorityScreenEvent) -> Unit
+) {
+    BaseDialogBuilder.BaseDialog(
+        onDismissRequest = { onEvent(PickTaskPriorityScreenEvent.OnDismissRequest) },
+        actionButtons = BaseDialogBuilder.ActionButtons(
+            confirmButton = {
+                BaseDialogBuilder.BaseActionButton(
+                    text = "Confirm",
+                    enabled = state.canConfirm,
+                    onClick = {
+                        onEvent(PickTaskPriorityScreenEvent.OnConfirmClick)
+                    }
+                )
+            },
+            dismissButton = {
+                BaseDialogBuilder.BaseActionButton(
+                    text = "Cancel",
+                    onClick = {
+                        onEvent(PickTaskPriorityScreenEvent.OnDismissRequest)
+                    }
+                )
+            }
+        )
+    ) {
+        BaseTextFiledBuilder.BaseOutlinedTextField(
+            value = state.inputPriority,
+            onValueChange = { onEvent(PickTaskPriorityScreenEvent.OnInputUpdatePriority(it)) },
+            valueValidator = state.valueValidator,
+            singleLine = true,
+            label = {
+                Text(text = "priority")
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            modifier = Modifier.fillMaxWidth(),
+        )
+    }
+}
---
+++ androidApp/src/main/java/com/example/inhabitnow/android/presentation/create_edit_task/common/config/pick_priority/PickTaskPriorityStateHolder.kt
@@ -0,0 +1,85 @@
+package com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority
+
+import com.example.inhabitnow.android.presentation.base.state_holder.BaseResultStateHolder
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenEvent
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenResult
+import com.example.inhabitnow.android.presentation.create_edit_task.common.config.pick_priority.components.PickTaskPriorityScreenState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+
+class PickTaskPriorityStateHolder(
+    initPriority: String,
+    override val holderScope: CoroutineScope
+) : BaseResultStateHolder<PickTaskPriorityScreenEvent, PickTaskPriorityScreenState, PickTaskPriorityScreenResult>() {
+
+    private val inputPriorityState = MutableStateFlow(initPriority)
+
+    private val valueValidator: (String) -> Boolean = { input ->
+        input.isEmpty() || validate(input)
+    }
+
+    private val canConfirmState = inputPriorityState.map { input ->
+        validate(input)
+    }.stateIn(
+        holderScope,
+        SharingStarted.Eagerly,
+        false
+    )
+
+    override val uiScreenState: StateFlow<PickTaskPriorityScreenState> =
+        combine(inputPriorityState, canConfirmState) { inputPriority, canConfirm ->
+            PickTaskPriorityScreenState(
+                inputPriority = inputPriority,
+                canConfirm = canConfirm,
+                valueValidator = valueValidator
+            )
+        }.stateIn(
+            holderScope,
+            SharingStarted.WhileSubscribed(5000L),
+            PickTaskPriorityScreenState(
+                inputPriority = inputPriorityState.value,
+                canConfirm = canConfirmState.value,
+                valueValidator = valueValidator
+            )
+        )
+
+    override fun onEvent(event: PickTaskPriorityScreenEvent) {
+        when (event) {
+            is PickTaskPriorityScreenEvent.OnInputUpdatePriority ->
+                onInputUpdatePriority(event)
+
+            is PickTaskPriorityScreenEvent.OnConfirmClick -> onConfirmClick()
+            is PickTaskPriorityScreenEvent.OnDismissRequest -> onDismissRequest()
+        }
+    }
+
+    private fun onInputUpdatePriority(event: PickTaskPriorityScreenEvent.OnInputUpdatePriority) {
+        inputPriorityState.update { event.value }
+    }
+
+    private fun onConfirmClick() {
+        if (canConfirmState.value) {
+            setUpResult(PickTaskPriorityScreenResult.Confirm(inputPriorityState.value))
+        }
+    }
+
+    private fun onDismissRequest() {
+        setUpResult(PickTaskPriorityScreenResult.Dismiss)
+    }
+
+    private fun validate(inputPriority: String): Boolean = inputPriority.toIntOrNull()?.let {
+        it in MIN_PRIORITY..MAX_PRIORITY
+    } ?: false
+
+    companion object {
+        private const val MIN_PRIORITY = 0
+        private const val MAX_PRIORITY = 999
+    }
+
+}
